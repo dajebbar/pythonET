1f0b5af7e56f8dd40d61ce18a2914567899b4086
dajebbar <dajebbar@gmx.fr>
2022-08-29 11:32:00 +0100

    game map

--- python fundamentals/games.py
+++ python fundamentals/games.py
@@ -3,14 +3,18 @@ game = [[0, 0, 0],
         [0, 0, 0],]
 
 
-def game_board(player=0, row=0, column=0, just_display=False):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
         print("   0  1  2")
         if not just_display:
-                game[row][column] = player
-        for count, row in enumerate(game):
+                game_map[row][column] = player
+        for count, row in enumerate(game_map):
                 print(count, row)
+                
+        return game_map     
 
-game_board(just_display=True)
+game = game_board(game, just_display=True)
+# print(game)
 print()
-game_board(2, 1, 1)
+game = game_board(game, 2, 1, 1)
 # game[1][1] = 1
+# print(game)
