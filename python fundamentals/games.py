54e667d73beb87b0a750a4e05375960832388e4b
dajebbar <dajebbar@gmx.fr>
2022-08-29 11:57:32 +0100

    TypeError

--- python fundamentals/games.py
+++ python fundamentals/games.py
@@ -12,13 +12,15 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
                         print(count, row)
                         
                 return game_map  
-        except IndexError:
-                print('values entered are out of range') 
-                return False  
+        except IndexError as e:
+                print(f'Error: meke sure you input row/column as 0 1 or 2! {e}') 
+                return False
+        except Exception as e:
+                print(f'Something went wrong! {e}')
 
 game = game_board(game, just_display=True)
 # print(game)
 print()
-game = game_board(game, 2, 5, 1)
+game = game_board(game_board, 2, 5, 1)
 # game[1][1] = 1
 # print(game)
