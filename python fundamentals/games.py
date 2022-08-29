6cc6b4077a554c3f24c0457adf5ea3b5552cc416
dajebbar <dajebbar@gmx.fr>
2022-08-29 09:03:13 +0100

    game_board parameters

--- python fundamentals/games.py
+++ python fundamentals/games.py
@@ -3,11 +3,12 @@ game = [[0, 0, 0],
         [0, 0, 0],]
 
 
-def game_board():
+def game_board(player=0, row=0, column=0):
         print("   0  1  2")
+        game[row][column] = player
         for count, row in enumerate(game):
                 print(count, row)
 
-game_board()
-game[1][1] = 1
+game_board(2, 1, 1)
+# game[1][1] = 1
 game_board()
