1e7ff6f870a0c390d64b7929923ca0eee1e39f04
dajebbar <dajebbar@gmx.fr>
2022-08-29 11:21:20 +0100

    mutability and global

--- python fundamentals/mutability.py
+++ python fundamentals/mutability.py
@@ -1,12 +1,16 @@
 game = 'I want to play a game'
-
+# game = [1, 2, 3]
+print(id(game))
 def board():
     # print(game)
     global game 
     game = 'game'
+    # game[1] = -99
     print(game)
+    print(id(game))
     
 
 print(game)
 board()
 print(game)
+print(id(game))
