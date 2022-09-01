77276bd22e023485a50d3d65d8697b22af725623
dajebbar <dajebbar@gmx.fr>
2022-09-01 13:06:06 +0100

    diagonal winner

--- python fundamentals/games.py
+++ python fundamentals/games.py
@@ -1,6 +1,6 @@
-game = [[1, 0, 1],
+game = [[2, 0, -2],
         [1, 2, 1],
-        [0, 2, 1],]
+        [-2, 2, 2],]
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
@@ -23,15 +23,21 @@ def win(game):
                 # print(row)
                 if row.count(row[0]) == len(row) and row[0] != 0:
                         print('winner!')
-                
+        check = []       
         for col in range(len(game)):
-                check = []
                 for row in game:
                         check.append(row[col])
                 if check.count(check[0]) == len(check) and check[0] != 0:
                         print('winner!')
-                
                         
+        diags = []
+        for i in range(len(game)):
+            diags.append(game[i][i])
+        if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+                    print('winner!')
+                #     break
+            
+        
                 
         
 
