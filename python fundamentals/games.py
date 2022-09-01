d13c18682a3d1e838fda575e25a100cfcc1df00a
dajebbar <dajebbar@gmx.fr>
2022-09-01 13:23:43 +0100

    test

--- python fundamentals/games.py
+++ python fundamentals/games.py
@@ -1,6 +1,6 @@
-game = [[2, 0, -2],
-        [1, -2, 1],
-        [-2, 2, 2],]
+game = [[2, 1, 2],
+        [0, 1, 1],
+        [2, 1, 2],]
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
@@ -24,9 +24,9 @@ def win(game):
                 # print(row)
                 if row.count(row[0]) == len(row) and row[0] != 0:
                         print(f"Player {row[0]} is the winner horizontally!")
-        # Vertical
-        check = []       
+        # Vertical      
         for col in range(len(game)):
+                check = [] 
                 for row in game:
                         check.append(row[col])
                 if check.count(check[0]) == len(check) and check[0] != 0:
