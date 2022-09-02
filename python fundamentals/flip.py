eebab589a5c6eb92ca50c0ca5bef079ee835bc70
dajebbar <dajebbar@gmx.fr>
2022-09-02 11:14:34 +0100

    other method for cycling players

--- python fundamentals/flip.py
+++ python fundamentals/flip.py
@@ -1,7 +1,15 @@
 players = [1, 0]
 choice = 1
 
-for i in range(10):
+for _ in range(10):
     current_player = choice + 1
-    print(current_player)
+    # print(current_player)
     choice = players[choice]
+
+
+# Other method
+import itertools
+
+player_choice = itertools.cycle(range(1, 3))
+for _ in range(10):
+    print(next(player_choice))
