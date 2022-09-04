0b5a33c3d3f0a55f6484d03bb107222298e3d747
dajebbar <dajebbar@gmx.fr>
2022-09-04 08:57:36 +0100

    formatting

--- python adv/strings_op.py
+++ python adv/strings_op.py
@@ -14,5 +14,12 @@ file_loc = 'loc'
 file_name = 'exp.txt'
 # print(file_loc + '/' + file_name) # not the correct method
 
-with open(os.path.join(file_loc, file_name)) as f:
+with open(os.path.join(file_loc, file_name)) as f: # better way
     print(f.read())
+
+
+# Formatting
+who = 'gary'
+how_many = 12
+print(who, ' bought ', how_many, ' apples today!') 
+print(f'{who} bought {how_many} apples today!')
