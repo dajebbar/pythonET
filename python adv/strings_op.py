b81167aeac96ec70e0086e8ef512e84c69a00e20
dajebbar <dajebbar@gmx.fr>
2022-09-04 08:35:43 +0100

    join scale better in concatenation

--- python adv/strings_op.py
+++ python adv/strings_op.py
@@ -3,3 +3,7 @@ names = ['Jeff', 'Gary', 'Jill', 'Samantha']
 for name in names:
     statement = 'Hello there ' + name # This is easily read, but unfortunately not the ideal choice
     print(statement)
+
+for name in names:
+    statement = ' '.join(['Hello there ', name]) # join() scales better
+    print(statement)
