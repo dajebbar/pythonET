54e5ce9a219a040d6d06d18674e5de39a354e4b9
dajebbar <dajebbar@gmx.fr>
2022-09-04 08:48:11 +0100

    os.path.join concat

--- python adv/strings_op.py
+++ python adv/strings_op.py
@@ -8,4 +8,11 @@ for name in names:
     statement = ' '.join(['Hello there ', name]) # join() scales better
     # print(statement)
     
+import os
 
+file_loc = 'loc'
+file_name = 'exp.txt'
+# print(file_loc + '/' + file_name) # not the correct method
+
+with open(os.path.join(file_loc, file_name)) as f:
+    print(f.read())
