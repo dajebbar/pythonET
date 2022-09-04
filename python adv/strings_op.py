4001ad790bd40577980bf0a1e7321beb7cdce501
dajebbar <dajebbar@gmx.fr>
2022-09-04 08:32:31 +0100

    not ideal choice concatenation

---
+++ python adv/strings_op.py
@@ -0,0 +1,5 @@
+names = ['Jeff', 'Gary', 'Jill', 'Samantha']
+
+for name in names:
+    statement = 'Hello there ' + name # This is easily read, but unfortunately not the ideal choice
+    print(statement)
