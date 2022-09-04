32d9c6010a1d3ee2da16dcedfea2721710f4b6a9
dajebbar <dajebbar@gmx.fr>
2022-09-04 08:44:53 +0100

    file path bad way concat

--- python adv/strings_op.py
+++ python adv/strings_op.py
@@ -2,8 +2,15 @@ names = ['Jeff', 'Gary', 'Jill', 'Samantha']
 
 for name in names:
     statement = 'Hello there ' + name # This is easily read, but unfortunately not the ideal choice
-    print(statement)
+    # print(statement)
 
 for name in names:
     statement = ' '.join(['Hello there ', name]) # join() scales better
-    print(statement)
+    # print(statement)
+    
+
+import os
+
+file_loc = 'python adv/loc'
+file_name = 'exp.txt'
+print(file_loc + '/' + file_name) # not the correct method
