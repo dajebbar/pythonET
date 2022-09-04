2f4c5b642d3580f3135e775366d3db4ab112a0fb
dajebbar <dajebbar@gmx.fr>
2022-09-04 09:28:26 +0100

    argparse

--- python adv/argparse_cli.py
+++ python adv/argparse_cli.py
@@ -1,15 +1,42 @@
-def calc(x, y, operation):
-    if operation == 'add':
-        return x + y
-    elif operation == 'sub':
-        return x - y
-    elif operation == 'mul':
-        return x * y
-    elif operation == 'div':
+import sys
+import argparse
+
+
+def calc(args):
+    if args.operation == 'add':
+        return args.x + args.y
+    elif args.operation == 'sub':
+        return args.x - args.y
+    elif args.operation == 'mul':
+        return args.x * args.y
+    elif args.operation == 'div':
         try:
-            return x / y
+            return args.x / args.y
         except ZeroDivisionError:
             return f'Error! {y} must be different of zero.'
 
-operation = 'mul'
-print(calc(5, 5, operation))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', 
+                        type=float, 
+                        default=1.0, 
+                        help='What is the first number?'
+                        )
+    parser.add_argument('--y', 
+                        type=float, 
+                        default=1.0, 
+                        help='What is the second number?'
+                        )
+    parser.add_argument('--operation', 
+                        type=str, 
+                        default='add', 
+                        help='What operation? Can choose add, sub, mul, or div'
+                        )
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
+
+if __name__=='__main__':
+    main()
+
+# operation = 'mul'
+# print(calc(5, 5, operation))
