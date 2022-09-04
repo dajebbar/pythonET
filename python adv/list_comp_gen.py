7ee7bc3e0f7905b376372a12d610a7d7502054c3
dajebbar <dajebbar@gmx.fr>
2022-09-04 09:55:59 +0100

    use case generator

---
+++ python adv/list_comp_gen.py
@@ -0,0 +1,10 @@
+input_list = [5, 2, 3, 15, 10, 18, 20, 25, 7]
+
+def div_by_five(x):
+    if x % 5 == 0:
+        return True
+    else:
+        return False
+    
+xyz = (i for i in input_list if div_by_five(i))
+[print(i) for i in xyz]
